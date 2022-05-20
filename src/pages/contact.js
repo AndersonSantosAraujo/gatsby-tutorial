e181d1d98eaedd0bc9a7df90552241ddce3d4562
Anderson <91352623+AndersonSantosAraujo@users.noreply.github.com>
2022-05-20 01:16:19 -0300

    Update contect.js
    Update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,40 +17,44 @@ const Contact = () => {
             <p>{text}</p>
           </div>
           <div className="col-md-6 mb-3">
-            <form>
+            <form name="contact" method="POST" data-netlify="true">
+              <input type="hidden" name="form-name" value="contact" />
               <div className="mb-3">
-                <label for="exampleInputEmail1" className="form-label">
-                  Email address
+                <label for="exampleInputName" className="form-label">
+                  Name
                 </label>
                 <input
+                  name="name"
+                  type="text"
+                  className="form-control"
+                  id="exampleInputName"
+                />
+              </div>
+              <div className="mb-3">
+                <label for="exampleInputEmail" className="form-label">
+                  Email
+                </label>
+                <input
+                  name="email"
                   type="email"
                   className="form-control"
-                  id="exampleInputEmail1"
+                  id="exampleInputEmail"
                   aria-describedby="emailHelp"
                 />
                 <div id="emailHelp" className="form-text">
                   We'll never share your email with anyone else.
                 </div>
               </div>
-              <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">
-                  Password
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">
+                  Example textarea
                 </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                />
-              </div>
-              <div className="mb-3 form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label className="form-check-label" for="exampleCheck1">
-                  Check me out
-                </label>
+                <textarea
+                  name="message"
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                ></textarea>
               </div>
               <button type="submit" className="btn btn-gatsby text-white">
                 Submit
